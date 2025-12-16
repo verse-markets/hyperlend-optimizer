73d12b46fa9766556fa68b987ed7131ec5a2c216
msinkec <sinkec.mihael@gmail.com>
2025-12-16 09:29:40 +0100

    feat: handle assets with null addresses for stable debt token properly

--- src/PositionsManagerInternal.sol
+++ src/PositionsManagerInternal.sol
@@ -125,8 +125,10 @@ abstract contract PositionsManagerInternal is MatchingEngine {
 
             uint256 trueP2PBorrow = market.trueP2PBorrow(indexes);
             uint256 borrowCap = config.getBorrowCap() * (10 ** config.getDecimals());
-            uint256 poolDebt =
-                ERC20(market.variableDebtToken).totalSupply() + ERC20(market.stableDebtToken).totalSupply();
+            uint256 poolDebt = ERC20(market.variableDebtToken).totalSupply();
+            if (market.stableDebtToken != address(0)) {
+                poolDebt += ERC20(market.stableDebtToken).totalSupply();
+            }
 
             if (amount + trueP2PBorrow + poolDebt > borrowCap) revert Errors.ExceedsBorrowCap();
         }
